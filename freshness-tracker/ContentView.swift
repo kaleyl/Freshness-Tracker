e23a91a2ab85e20bc68816bbc9c64e5aee6d37f3
ChingYi Leung <kkaleyleung@berkeley.edu>
2020-04-09 01:09:48 -0700

    first commit

---
+++ freshness-tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  freshness-tracker
+//
+//  Created by Kaley Leung on 4/9/20.
+//  Copyright © 2020 Kaley Leung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
