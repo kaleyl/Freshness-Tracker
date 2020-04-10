a7cd714b9c7e3dc1475305f78b47375e20a14e35
ChingYi Leung <kkaleyleung@berkeley.edu>
2020-04-09 22:16:55 -0700

    tab bar and tracker table view prototype

---
+++ View Controllers/FoodEntryCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodEntryCell.swift
+//  freshness-tracker
+//
+//  Created by Kaley Leung on 4/9/20.
+//  Copyright © 2020 Kaley Leung. All rights reserved.
+//
+
+import UIKit
+
+class FoodEntryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
