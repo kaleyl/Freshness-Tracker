fd6367a13ef6988158b74683a67b353f37e7c6b9
ChingYi Leung <kkaleyleung@berkeley.edu>
2020-04-20 21:50:33 -0700

    list cell

--- freshness-tracker/View/WishListItemCell.swift
+++ freshness-tracker/View/WishListItemCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class WishListItemCell: UITableViewCell {
 
+    @IBOutlet weak var checkButton: UIButton!
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
